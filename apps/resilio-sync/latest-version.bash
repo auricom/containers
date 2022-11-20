42f38d5c00662204e2c6990c8dc822f65a488361
auricom <27022259+auricom@users.noreply.github.com>
2022-11-20 00:41:37 +0000

    🩹 fix resilio package url

--- apps/resilio-sync/latest-version.bash
+++ apps/resilio-sync/latest-version.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version="$(curl -sX GET http://linux-packages.resilio.com/resilio-sync/deb/dists/resilio-sync/non-free/binary-amd64/Packages |grep -A 7 -m 1 'Package: resilio-sync' | awk -F ': ' '/Version/{print $2;exit}')"
+version="$(curl -sX GET https://linux-packages.resilio.com/resilio-sync/deb/dists/resilio-sync/non-free/binary-amd64/Packages |grep -A 7 -m 1 'Package: resilio-sync' | awk -F ': ' '/Version/{print $2;exit}')"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
