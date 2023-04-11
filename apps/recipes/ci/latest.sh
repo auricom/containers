90b1a92c35515298e17eb7f4fe70cea25d3c170b
auricom <27022259+auricom@users.noreply.github.com>
2023-04-11 23:45:05 +0200

    🔥 recipes

--- apps/recipes/ci/latest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-version="$(curl -sX GET "https://api.github.com/repos/TandoorRecipes/recipes/releases/latest" | jq --raw-output '.tag_name')"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
