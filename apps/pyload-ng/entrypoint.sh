d33d56b51f4fc941f4559021e9ee4652e99eb7a9
auricom <27022259+auricom@users.noreply.github.com>
2024-01-07 20:53:50 +0100

    🚑 pyloadng remove ensurepip

--- apps/pyload-ng/entrypoint.sh
+++ apps/pyload-ng/entrypoint.sh
@@ -3,6 +3,9 @@
 #shellcheck disable=SC1091
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 
+#shellcheck disable=SC1091
+source /app/venv/bin/activate
+
 #shellcheck disable=SC2086
 exec \
     pyload --userdir /config
