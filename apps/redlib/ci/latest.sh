2b223c3e94c5c3305732f33ba1ae38f3ee652f91
auricom <27022259+auricom@users.noreply.github.com>
2024-10-23 17:19:03 +0200

    feat: redlib rolling release

--- apps/redlib/ci/latest.sh
+++ apps/redlib/ci/latest.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-version="$(curl -sX GET "https://api.github.com/repos/redlib-org/redlib/releases/latest" | jq --raw-output '.tag_name' || true)"
+# version="$(curl -sX GET "https://api.github.com/repos/redlib-org/redlib/releases/latest" | jq --raw-output '.tag_name' || true)"
+version="$(curl -sX GET "https://api.github.com/repos/redlib-org/redlib/commits/main" | jq --raw-output '.sha')"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
