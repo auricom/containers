134e471d18a81d2bfb16fc0b35c40aceb253857a
auricom <27022259+auricom@users.noreply.github.com>
2025-01-20 00:15:22 +0100

    feat: github-notifier

---
+++ apps/github-notifier/ci/latest.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+version="$(curl -sX GET "https://api.github.com/repos/auricom/containers/commits?path=apps/github-notifier&per_page=1" | jq --raw-output '.[0].sha')"
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
