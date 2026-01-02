3e5e66dcb7fa03e48246cc746215c275f0ee9258
auricom <27022259+auricom@users.noreply.github.com>
2026-01-02 13:45:26 +0100

    chore: rework

--- apps/kresus/ci/latest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-version="$(curl -sX GET "https://registry.npmjs.org/kresus/latest" | jq -j -r '.version')"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
