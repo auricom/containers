a234bc50e977568ad648545a091aa7ed29c4e7a1
auricom <27022259+auricom@users.noreply.github.com>
2026-02-02 03:02:58 +0100

    fix: redlib tmp branch

--- apps/redlib/ci/latest.sh
+++ apps/redlib/ci/latest.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # version="$(curl -sX GET "https://api.github.com/repos/redlib-org/redlib/releases/latest" | jq --raw-output '.tag_name' || true)"
-version="$(curl -sX GET "https://api.github.com/repos/redlib-org/redlib/commits/main" | jq --raw-output '.sha')"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+# version="$(curl -sX GET "https://api.github.com/repos/redlib-org/redlib/commits/main" | jq --raw-output '.sha')"
+# version="${version#*v}"
+# version="${version#*release-}"
+# printf "%s" "${version}"
+printf "%s" "42fe41bc4c64690aa91cd1cfecec3bad3438354f"
