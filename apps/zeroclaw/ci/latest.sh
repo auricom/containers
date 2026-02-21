4159b543517f82b9a1b33b1bfdb84bebdd39be1f
auricom <27022259+auricom@users.noreply.github.com>
2026-02-21 22:01:17 +0100

    chore: zeroclaw use releases

--- apps/zeroclaw/ci/latest.sh
+++ apps/zeroclaw/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version="$(curl -sX GET "https://api.github.com/repos/zeroclaw-labs/zeroclaw/commits?per_page=1" | jq --raw-output '.[0].sha')"
+version="$(curl -sX GET "https://api.github.com/repos/zeroclaw-labs/zeroclaw/releases/latest" | jq --raw-output '.tag_name')"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
