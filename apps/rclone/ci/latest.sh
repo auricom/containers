043c7dd3454ad7ca884c44190037ecacf4c8f860
auricom <27022259+auricom@users.noreply.github.com>
2026-02-22 22:34:37 +0100

    feat: toolbox

--- apps/rclone/ci/latest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-file=/tmp/rclone.yaml
-
-curl -fsSL -o "${file}" https://raw.githubusercontent.com/wolfi-dev/os/main/rclone.yaml
-version="$(yq .package.version ${file})"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
-rm "${file}"
